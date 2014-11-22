904f7220d088500edca4ad8c6e86fa6c4c4cdb0d
Brad Denver <brad.denver@gmail.com>
2014-11-22 21:33:56 +1100

    updated test to check creation of robot with the required (but empty) place, move, left, right and report methods

--- ruby/robot.rb
+++ ruby/robot.rb
@@ -1,3 +1,27 @@
 class Robot
 
+  # create a robot
+  def initialize
+  end
+
+  # place the robot on the board
+  def place
+  end
+
+  # move the robot 1 space in its current direction
+  def move
+  end
+
+  # rotate the robot 90 deg counter clockwise
+  def left
+  end
+
+  # rotate the robot 90 deg clockwise
+  def right
+  end
+
+  # announce the robots current coordinates and facing direction
+  def report
+  end
+
 end
--- ruby/robot_spec.rb
+++ ruby/robot_spec.rb
@@ -1,9 +1,18 @@
-require './robot.rb'
+require_relative './robot.rb'
 
 RSpec.describe Robot do
-  describe "test test" do
-    it "is a runnable test" do
-      expect(true).to eq(true)
+  
+  describe "#new" do
+    before :all do
+      @robot = Robot.new
     end
+    subject { @robot }
+    it { is_expected.to be_an_instance_of Robot }
+    it { is_expected.to respond_to :place }
+    it { is_expected.to respond_to :move }
+    it { is_expected.to respond_to :left }
+    it { is_expected.to respond_to :right }
+    it { is_expected.to respond_to :report }
   end
+
 end
